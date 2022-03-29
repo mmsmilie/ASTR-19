f346ce7314a8af6be45f511765c94955087aac43
mmsmilie <97249265+mmsmilie@users.noreply.github.com>
2022-03-29 10:12:51 -0700

    Add Session 1 Prompt.py

---
+++ Session 1 Prompt.py
@@ -0,0 +1,3 @@
+print("Hello my full name is Michael Muzilla.")
+print("My current favorite movie, or movie set is teh lord of the rings trilogy.")
+print("My favorite food is either a Habit double char burger or a double double from In n Out.")
