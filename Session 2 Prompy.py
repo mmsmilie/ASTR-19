a82806e8dacfd7d9cf11979a3df3b232ad30f2e9
mmsmilie <97249265+mmsmilie@users.noreply.github.com>
2022-04-15 10:06:14 -0700

    Add files via upload

---
+++ Session 2 Prompy.py
@@ -0,0 +1,19 @@
+from itertools import product
+
+
+floaty1 = 35.0
+floaty2 = 47.0
+inty1 = 7
+inty2 = 5
+#Sum
+sum1 = (floaty1 + floaty2)
+print(sum1)
+print (type(sum1))
+#Difference
+difference1 = inty1 + inty2
+print(difference1)
+print(type(difference1))
+#Product
+product1 = floaty1 * inty1
+print(product1)
+print(type(product1))
